705888d9f089500bc97355c222e90f2001b92500
Dima Z <dmitry.zabello@gmail.com>
2026-04-16 16:20:54 -0700

    Split client bundle into manual chunks

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,37 @@ import { devvit } from '@devvit/start/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+function manualChunks(id: string): string | undefined {
+  if (id.includes('node_modules/zrender')) {
+    return 'zrender';
+  }
+
+  if (id.includes('node_modules/echarts')) {
+    return 'echarts';
+  }
+
+  if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) {
+    return 'react';
+  }
+
+  if (id.includes('node_modules/@devvit')) {
+    return 'devvit';
+  }
+}
+
 export default defineConfig({
-  plugins: [react(), devvit()],
+  plugins: [
+    react(),
+    devvit({
+      client: {
+        build: {
+          rollupOptions: {
+            output: {
+              manualChunks,
+            },
+          },
+        },
+      },
+    }),
+  ],
 });
